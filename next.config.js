2b61ff4efc1196ea88a96eebd7f6c0a1a9286f0e
Money Mafia Manager <97112807+MoneyMafiaManager@users.noreply.github.com>
2022-04-10 17:39:49 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const baseUrl = '';
+
+module.exports = withBundleAnalyzer({
+  poweredByHeader: false,
+  trailingSlash: true,
+  basePath: baseUrl,
+  env: {
+    baseUrl,
+  },
+  reactStrictMode: true,
+});
