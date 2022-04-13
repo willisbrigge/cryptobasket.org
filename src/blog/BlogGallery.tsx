7b620b1bb2aac513f1c71de1c764ea431c0b00d8
Money Mafia Manager <97112807+MoneyMafiaManager@users.noreply.github.com>
2022-04-14 04:32:54 +0530

    revert

--- src/blog/BlogGallery.tsx
+++ src/blog/BlogGallery.tsx
@@ -16,15 +16,15 @@ const BlogGallery = (props: IBlogGalleryProps) => (
     <section className="p-5 md:p-0 grid grid-cols-1 md:grid-cols-1 xl:grid-cols-2 gap-5 items-start ">
       {props.posts.map((elt) => (
         <div key={elt.slug}>
-          <section className="p-5 py-5 text-left transform duration-1000 hover:-translate-y-2 hover:shadow-2xl cursor-pointer">
-            <h2 className="font-semibold mb-2 mt-5 text-cyan-600 uppercase">
-              <a href={'https://www.google.com/search?q=site%3Acryptobasket.org+' + elt.network}> #{elt.network}</a> | <a href={'https://www.google.com/search?q=site%3Acryptobasket.org+' + elt.category}> #{elt.category}</a>
-            </h2>
-            <Link href="/posts/[slug]" as={`/posts/${elt.slug}`} passHref>
+          <Link href="/posts/[slug]" as={`/posts/${elt.slug}`} passHref>
+            <section className="p-5 py-5 text-left transform duration-1000 hover:-translate-y-2 hover:shadow-2xl cursor-pointer">
+              <h2 className="font-semibold mb-2 mt-5 text-cyan-600 uppercase">
+                #{elt.network} | #{elt.category}
+              </h2>
               <h1 className="text-3xl mb-2 h-16">{elt.title}</h1>
               <p className="mb-5">{elt.description.substring(0, 150)}</p>
-            </Link>
-          </section>
+            </section>
+          </Link>
 
           <Link href="/posts/[slug]" as={`/posts/${elt.slug}`} passHref>
             <a className='hidden'>
