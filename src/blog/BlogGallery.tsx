f6b675aa37b9c3c3b3a6959ad1db50858296253f
besoeasy <besoeasy@users.noreply.github.com>
2022-04-30 21:38:26 +0530

    UI UPDATE

--- src/blog/BlogGallery.tsx
+++ src/blog/BlogGallery.tsx
@@ -6,54 +6,60 @@ import { Pagination, IPaginationProps } from '../pagination/Pagination';
 import { PostItems } from '../utils/Content';
 
 export type IBlogGalleryProps = {
-  posts: PostItems[];
-  pagination: IPaginationProps;
+	posts: PostItems[];
+	pagination: IPaginationProps;
 };
 
-
 const BlogGallery = (props: IBlogGalleryProps) => (
-  <>
-    <section className="p-5 md:p-0 grid grid-cols-1 md:grid-cols-1 xl:grid-cols-2 gap-5 items-start ">
-      {props.posts.map((elt) => (
-        <div key={elt.slug}>
-          <Link href="/posts/[slug]" as={`/posts/${elt.slug}`} passHref>
-            <section className="p-5 py-5 text-left transform duration-1000 hover:-translate-y-2 hover:shadow-2xl cursor-pointer">
-              <h2 className="font-semibold mb-2 mt-5 text-cyan-600 uppercase">
-                #{elt.network} | #{elt.category}
-              </h2>
-              <h1 className="text-3xl mb-2 h-16">{elt.title}</h1>
-              <p className="mb-5">{elt.description.substring(0, 150)}</p>
-            </section>
-          </Link>
+	<>
+		<section className="p-5 md:p-0 grid grid-cols-1 md:grid-cols-1 xl:grid-cols-2 gap-5 items-start ">
+			{props.posts.map((elt) => (
+				<div key={elt.slug}>
+					<Link href="/posts/[slug]" as={`/posts/${elt.slug}`} passHref>
+						<section className="relative block group h-80 cursor-pointer">
+							<span className="absolute inset-0 border-2 border-black border-dashed" />
+							<div className="relative flex items-end h-full transition-transform transform bg-white border-2 border-black group-hover:-translate-x-2 group-hover:-translate-y-2">
+								<div className="px-8 pb-8 transition-opacity group-hover:opacity-0 group-hover:absolute">
+									<h2 className="font-semibold mb-2 mt-5 text-cyan-600 uppercase">
+										#{elt.network} | #{elt.category}
+									</h2>
+									<h2 className="mt-4 text-2xl font-medium">{elt.title}</h2>
+								</div>
+								<div className="absolute p-8 transition-opacity opacity-0 group-hover:opacity-100 group-hover:relative">
+									<h2 className="mt-4 text-2xl font-medium">{elt.title}</h2>
+									<p className="mt-4">{elt.description.substring(0, 150)}</p>
+									<p className="mt-8 font-bold">Read more</p>
+								</div>
+							</div>
+						</section>
+					</Link>
 
-          <Link href="/posts/[slug]" as={`/posts/${elt.slug}`} passHref>
-            <a className='hidden'>
-              <h1>{elt.title}</h1>
-              <h2>{elt.description}</h2>
-            </a>
-          </Link>
+					<Link href="/posts/[slug]" as={`/posts/${elt.slug}`} passHref>
+						<a className="hidden">
+							<h1>{elt.title}</h1>
+							<h2>{elt.description}</h2>
+						</a>
+					</Link>
+					<Link href="/posts/[slug]" as={`/posts/${elt.slug}`} passHref>
+						<a className="hidden">
+							<h1>
+								{elt.network} {elt.category} {elt.title}
+							</h1>
+						</a>
+					</Link>
+					<Link href="/posts/[slug]" as={`/posts/${elt.slug}`} passHref>
+						<a className="hidden">
+							<h1>
+								{elt.title} {elt.network}
+							</h1>
+						</a>
+					</Link>
+				</div>
+			))}
+		</section>
 
-          <Link href="/posts/[slug]" as={`/posts/${elt.slug}`} passHref>
-            <a className='hidden'>
-              <h1>{elt.network} {elt.category} {elt.title}</h1>
-            </a>
-          </Link>
-
-          <Link href="/posts/[slug]" as={`/posts/${elt.slug}`} passHref>
-            <a className='hidden'>
-              <h1>{elt.title} {elt.network}</h1>
-            </a>
-          </Link>
-
-        </div>
-      ))}
-    </section>
-
-    <Pagination
-      previous={props.pagination.previous}
-      next={props.pagination.next}
-    />
-  </>
+		<Pagination previous={props.pagination.previous} next={props.pagination.next} />
+	</>
 );
 
 export { BlogGallery };
