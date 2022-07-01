6a74c4dcb4a2dcec4b1355c965527a3b66eb1d8a
Aman Singh Katal <besoeasy@users.noreply.github.com>
2022-07-01 15:49:34 +0530

    Update [slug].tsx

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -64,16 +64,6 @@ const DisplayPost = (props: IPostProps) => (
 		</div>
 		
                 <div id="disqus_thread"></div>
-
-<script>
-    (function() { // DON'T EDIT BELOW THIS LINE
-    var d = document, s = d.createElement('script');
-    s.src = 'https://cryptobasket.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-    })();
-</script>
-
 	</Main>
 );
 
