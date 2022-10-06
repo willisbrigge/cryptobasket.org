04577cf06565ff7aa5e2e8df3df0f08b67522f0c
Aman Singh Katal <besoeasy@users.noreply.github.com>
2022-10-07 00:18:16 +0530

    Update AppConfig.ts

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -6,5 +6,5 @@ export const AppConfig = {
 	locale: 'en',
 	author: 'Anonymous',
 	pagination_size: 15,
-	ipfs_gateway: 'https://ipfs.infura.io/ipfs/',
+	ipfs_gateway: 'https://infura-ipfs.io/ipfs/',
 };
