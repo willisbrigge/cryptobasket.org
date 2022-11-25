a412cc23b47f55a2a16e30b89e785aacd8fa869e
besoeasy <besoeasy@users.noreply.github.com>
2022-11-25 18:53:38 +0530

    my-10

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -37,14 +37,16 @@ const DisplayPost = (props: IPostProps) => (
 		}
 	>
 		<div className="text-center py-20">
-			<h2 className="relative inline-block px-5 py-2 mb-5 text-5xl  font-extrabold bg-white border-2 border-black">
-				<div className="absolute w-full py-2 h-full inset-0 border-2 border-black bg-black ml-1.5 mt-1.5" />
-				<div className="absolute inset-0 w-full h-full py-2 bg-white" />
-				<span className="relative">{props.title}</span>
-			</h2>
-			<p className="text-xl font-medium text-gray-800 mb-7">
-				#{props.category} - {format(new Date(props.date), 'LLLL d, yyyy')}
-			</p>
+			<div className="my-10">
+				<h2 className="relative inline-block px-5 py-2 mb-5 text-5xl  font-extrabold bg-white border-2 border-black">
+					<div className="absolute w-full py-2 h-full inset-0 border-2 border-black bg-black ml-1.5 mt-1.5" />
+					<div className="absolute inset-0 w-full h-full py-2 bg-white" />
+					<span className="relative">{props.title}</span>
+				</h2>
+				<p className="text-xl font-medium text-gray-800 mb-7">
+					#{props.category} - {format(new Date(props.date), 'LLLL d, yyyy')}
+				</p>
+			</div>{' '}
 		</div>
 
 		<div className="m-auto max-w-3xl">
